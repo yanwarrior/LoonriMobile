c1c8567f70ccfd41c47138997168bc090458390f
yanwarsolah <yanwarsolah@gmail.com>
2020-06-03 21:49:11 +0700

    Menghilangkan tanda + di toolbar acceptance list.

--- src/app/pages/acceptance-list/acceptance-list.module.ts
+++ src/app/pages/acceptance-list/acceptance-list.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { AcceptanceListPageRoutingModule } from './acceptance-list-routing.module';
 
 import { AcceptanceListPage } from './acceptance-list.page';
+import { CartCounterComponent } from 'src/app/components/cart-counter/cart-counter.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,9 @@ import { AcceptanceListPage } from './acceptance-list.page';
     IonicModule,
     AcceptanceListPageRoutingModule
   ],
-  declarations: [AcceptanceListPage]
+  declarations: [
+    AcceptanceListPage,
+    CartCounterComponent
+  ]
 })
 export class AcceptanceListPageModule {}
